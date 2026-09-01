48e6537703b83b6296c4dba367e3edc2e43d368e
ksasaki-mac <ks.koji.sasaki@gmail.com>
2026-09-01 20:30:35 +0900

    Initial commit

---
+++ DangoStack/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DangoStack
+//
+//  Created by EverGreen on 2026/09/01.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
